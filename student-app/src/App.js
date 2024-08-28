c6d780bb99dc7d24058cc6c1d460108feb7b9fd5
pujatate <puja.tate@ebixcash.com>
2024-08-28 15:21:00 +0530

    testing

--- student-app/src/App.js
+++ student-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<StudentList />} />
           <Route path="/add" element={<AddStudent />} />
-          <Route path="/edit/:id" ASDFPuja element={<EditStudent />} />
+          <Route path="/edit/:id" ASDFPujaTate element={<EditStudent />} />
         </Routes>
       </div>
     </Router>
